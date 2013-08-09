459365d1ddc6e5753a0e7a328723d146ab960eb6
Bence <kormos.bence@gmail.com>
2013-08-09 12:21:02 +0100

    Adds performance tests

---
+++ performance/js/dotNotation.js
@@ -0,0 +1,59 @@
+(function () {
+    var ns = {
+        com: {
+            benqus: {
+                my: {
+                    space: {
+                        variables: {}
+                    }
+                }
+            }
+        }
+    };
+
+    var test = {
+
+        iteration: 100000000,
+
+        tests: {
+            dots: function dot() {
+                var max = test.iteration;
+                var i = 0;
+                var holder;
+
+                while (i < max) {
+                    holder = ns.com.benqus.my.space.variables;
+                    i++;
+                }
+            },
+
+            brackets: function dot() {
+                var max = test.iteration;
+                var i = 0;
+                var holder;
+
+                while (i++ < max) {
+                    holder = ns["com"]["benqus"]["my"]["space"]["variables"];
+                    i++;
+                }
+            }
+        },
+
+        run: function (type) {
+            var start = Date.now();
+            var end;
+
+            this.tests[type]();
+
+            end = Date.now();
+
+            console.log(type + ": " + (end - start) + "ms");
+            console.log("---------");
+        }
+    };
+
+    window.onload = function () {
+        test.run("dots");
+        test.run("brackets");
+    };
+}());
