1d7c5be57c37c42d80cdfcecd4a14fb7c83346cf
Bence <kormos.bence@gmail.com>
2013-08-09 12:33:40 +0100

    Refactor for performance

--- performance/js/dotNotation.js
+++ performance/js/dotNotation.js
@@ -11,6 +11,14 @@
         }
     };
 
+    var ns2 = new (function () {
+        this.com = {};
+        this.com.benqus = {};
+        this.com.benqus.my = {};
+        this.com.benqus.my.space = {};
+        this.com.benqus.my.space.variables = {};
+    })();
+
     var test = {
 
         iteration: 100000000,
@@ -36,6 +44,50 @@
                     holder = ns["com"]["benqus"]["my"]["space"]["variables"];
                     i++;
                 }
+            },
+
+            dots2: function dot() {
+                var max = test.iteration;
+                var i = 0;
+                var holder;
+
+                while (i < max) {
+                    holder = ns2.com.benqus.my.space.variables;
+                    i++;
+                }
+            },
+
+            brackets2: function dot() {
+                var max = test.iteration;
+                var i = 0;
+                var holder;
+
+                while (i++ < max) {
+                    holder = ns2["com"]["benqus"]["my"]["space"]["variables"];
+                    i++;
+                }
+            },
+
+            dots3: function dot() {
+                var max = test.iteration;
+                var i = 0;
+                var holder;
+
+                while (i < max) {
+                    holder = ns2.com;
+                    i++;
+                }
+            },
+
+            brackets3: function dot() {
+                var max = test.iteration;
+                var i = 0;
+                var holder;
+
+                while (i++ < max) {
+                    holder = ns2["com"];
+                    i++;
+                }
             }
         },
 
@@ -52,7 +104,11 @@
     };
 
     window.onload = function () {
-        test.run("brackets");
         test.run("dots");
+        test.run("brackets");
+        test.run("dots2");
+        test.run("brackets2");
+        test.run("dots3");
+        test.run("brackets3");
     };
 }());
