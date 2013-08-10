f10ea771f8193da00ea8005ebfd0834616e063d2
Bence <kormos.bence@gmail.com>
2013-08-10 19:14:10 +0100

    Refactors and extends Drawing base class

--- gauges/build/gauges-0.1.0.js
+++ gauges/build/gauges-0.1.0.js
@@ -92,10 +92,20 @@
     
     gauges.Base = Base;
 
+    /**
+     * Abstract class for any drawing which will be drawn on the canvas.
+     * @type {Drawing}
+     */
     var Drawing = gauges.Drawing = Base.extend({
-        constructor: function (canvas) {
+        /**
+         * @constructor
+         * @param canvas {String|HTMLCanvasElement}
+         * @param isolated {Boolean} should draw the Drawing in an isolated context
+         */
+        constructor: function (canvas, isolated) {
             this.canvas = undefined;
             this.context = undefined;
+            this.isolated = (isolated === true);
     
             if (canvas) {
                 this.setCanvas(canvas);
@@ -114,6 +124,10 @@
             return this.context;
         },
     
+        /**
+         * @param canvas
+         * @returns {*}
+         */
         setCanvas: function (canvas) {
             if (canvas) {
                 this.canvas = getCanvas(canvas);
@@ -123,27 +137,72 @@
             return this;
         },
     
+        /**
+         * Isolates the Drawing in a separate coordinate system
+         * @returns {*}
+         */
+        isolate: function () {
+            this.isolated = true;
+            return this;
+        },
+    
+        /**
+         * Determines whether the Drawing is isolated
+         * @returns {*}
+         */
+        isIsolated: function () {
+            return this.isolated;
+        },
+    
+        /**
+         * Method should be overriden only in special cases
+         * @param context {CanvasRenderingContext2D} the context the drawing will be drawn on
+         * @returns {Drawing}
+         */
         beforeDraw: function (context) {
-            context.save();
+            if (this.isIsolated()) {
+                context.save();
+            }
+    
             return this;
         },
     
-        draw: function (/* context */) {
+        /**
+         * Draws the Drawing to the canvas. Method should be overriden.
+         * @param context {CanvasRenderingContext2D} the context the drawing will be drawn on
+         * @returns {Drawing}
+         */
+        draw: function (context) {
             return this;
         },
     
+        /**
+         * Method should be overriden only in special cases
+         * @param context {CanvasRenderingContext2D} the context the drawing will be drawn on
+         * @returns {Drawing}
+         */
         afterDraw: function (context) {
-            context.restore();
+            if (this.isIsolated()) {
+                context.restore();
+            }
+    
             return this;
         },
     
+        /**
+         * Method should not be used only by the framework.
+         * @returns {Drawing}
+         */
         render: function () {
             var context = this.getContext();
     
+            //logic before drawing
             this.beforeDraw(context);
     
+            //drawing logic
             this.draw(context);
     
+            //logic after drawing
             this.afterDraw(context);
     
             return this;
--- gauges/src/js/base/Drawing.js
+++ gauges/src/js/base/Drawing.js
@@ -1,7 +1,17 @@
+/**
+ * Abstract class for any drawing which will be drawn on the canvas.
+ * @type {Drawing}
+ */
 var Drawing = gauges.Drawing = Base.extend({
-    constructor: function (canvas) {
+    /**
+     * @constructor
+     * @param canvas {String|HTMLCanvasElement}
+     * @param isolated {Boolean} should draw the Drawing in an isolated context
+     */
+    constructor: function (canvas, isolated) {
         this.canvas = undefined;
         this.context = undefined;
+        this.isolated = (isolated === true);
 
         if (canvas) {
             this.setCanvas(canvas);
@@ -20,6 +30,10 @@ var Drawing = gauges.Drawing = Base.extend({
         return this.context;
     },
 
+    /**
+     * @param canvas
+     * @returns {*}
+     */
     setCanvas: function (canvas) {
         if (canvas) {
             this.canvas = getCanvas(canvas);
@@ -29,27 +43,72 @@ var Drawing = gauges.Drawing = Base.extend({
         return this;
     },
 
+    /**
+     * Isolates the Drawing in a separate coordinate system
+     * @returns {*}
+     */
+    isolate: function () {
+        this.isolated = true;
+        return this;
+    },
+
+    /**
+     * Determines whether the Drawing is isolated
+     * @returns {*}
+     */
+    isIsolated: function () {
+        return this.isolated;
+    },
+
+    /**
+     * Method should be overriden only in special cases
+     * @param context {CanvasRenderingContext2D} the context the drawing will be drawn on
+     * @returns {Drawing}
+     */
     beforeDraw: function (context) {
-        context.save();
+        if (this.isIsolated()) {
+            context.save();
+        }
+
         return this;
     },
 
-    draw: function (/* context */) {
+    /**
+     * Draws the Drawing to the canvas. Method should be overriden.
+     * @param context {CanvasRenderingContext2D} the context the drawing will be drawn on
+     * @returns {Drawing}
+     */
+    draw: function (context) {
         return this;
     },
 
+    /**
+     * Method should be overriden only in special cases
+     * @param context {CanvasRenderingContext2D} the context the drawing will be drawn on
+     * @returns {Drawing}
+     */
     afterDraw: function (context) {
-        context.restore();
+        if (this.isIsolated()) {
+            context.restore();
+        }
+
         return this;
     },
 
+    /**
+     * Method should not be used only by the framework.
+     * @returns {Drawing}
+     */
     render: function () {
         var context = this.getContext();
 
+        //logic before drawing
         this.beforeDraw(context);
 
+        //drawing logic
         this.draw(context);
 
+        //logic after drawing
         this.afterDraw(context);
 
         return this;
